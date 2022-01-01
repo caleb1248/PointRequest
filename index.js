056788d72ee3b1a67494bba0ef5aa7a35aa10a01
Caleb Liu <94559723+caleb1248@users.noreply.github.com>
2022-01-01 19:31:51 +0000

    send index.html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+	res.sendfile("index.html");
 });
 
 app.listen(3000, () => {
