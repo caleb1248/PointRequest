e272e2b75ead00fd4e6ed9b12865f4eb628127eb
Caleb Liu <94559723+caleb1248@users.noreply.github.com>
2022-01-01 20:01:32 +0000

    signup, login, and making the parent.html

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express');
-
+const express = require('express'),{createHash} = require("crypto"), fs = require("fs");
 const app = express();
-
-app.get('/', (req, res) => {
-	res.sendfile("index.html");
-});
+app.use(express.urlencoded());
+app.get('/', (req, res) => res.sendfile("index.html"));
 
 app.listen(3000, () => {
   console.log('server started');
 });
+app.get("/signup",(req,res) => res.sendfile("signup.html"));
+app.post("/signup",(req,res) => {
+	const code = createHash("sha256").update(req.body.secretcode).digest("hex");
+	fs.writeFileSync("parentcode.txt",code);
+	res.send("success");
+});
+app.post("/login",(req,res) => {
+	const code = createHash("sha256").update(req.body.secretcode).digest("hex");
+	if(code==fs.readFilesync("parentcode.txt")){
+		res.sendfile("parent.html")
+	}
+})
+
